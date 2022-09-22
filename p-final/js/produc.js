1ef9a183d8426562ea39a91d60b5c6eb35bf7307
Luciano Franchi <106634918+Lucianofranchi@users.noreply.github.com>
2022-09-21 23:27:57 -0300

    Add files via upload
    Progreso Proyecto Final.

---
+++ p-final/js/produc.js
@@ -0,0 +1,5 @@
+let stockProductos = [
+    prod = {id: 1, nombre : "Wilson", tipo : "Edicion 2000", encordado:"16x16", precio: 2000,imagen: "./img/WIlsson.jpg",},
+    prod = {id: 2, nombre : "Dunlop", tipo : "Edicion junior", encordado:"13x13", precio: 1200, imagen: "./img/DunlopJunior.jpg",},
+    prod = {id: 3, nombre : "Babolat", tipo : "Edicion limitada", encordado:"16x19", precio: 7000, imagen: "./img/Babolattt.jpg",},
+];
