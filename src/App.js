b77a4255c283fa53b82359a13461e849c908e191
Luciano Franchi <106634918+Lucianofranchi@users.noreply.github.com>
2022-09-29 18:23:02 -0300

    Add files via upload
    Navbar

---
+++ src/App.js
@@ -0,0 +1,14 @@
+import './App.css';
+
+import Avatar from './components/Avatar';
+import NavBar from './components/NavBar';
+
+function App() {
+  return (
+    <div className="App">
+      <Avatar src="https://via.placeholder.com/150x150"/>
+    </div>
+  );
+}
+
+export default App;
